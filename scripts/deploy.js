e6e383d5bc082063d46df8f09bb91883b4646c8f
avatariamfoundation-afk <avatariamfoundation@gmail.com>
2025-12-16 11:19:52 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,15 @@
 
+const hre = require("hardhat");
+
+async function main() {
+  const NeuroGridCore = await hre.ethers.getContractFactory("NeuroGridCore");
+  const contract = await NeuroGridCore.deploy();
+  await contract.deployed();
+
+  console.log("NeuroGridCore deployed to:", contract.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
