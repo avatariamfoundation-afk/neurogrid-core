40c82562f8d6440195a22d02c245a2eca0d3121b
avatariamfoundation-afk <avatariamfoundation@gmail.com>
2025-12-16 17:33:04 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
-networks: {
-  opbnbTestnet: {
-    url: process.env.OPBNB_TESTNET_RPC,
-    accounts: [process.env.PRIVATE_KEY],
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    opbnbTestnet: {
+      url: process.env.OPBNB_TESTNET_RPC,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
-},
-
+};
