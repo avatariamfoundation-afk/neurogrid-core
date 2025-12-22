3df13da5bbd771356fb181c7e040756afcad2c7f
Aethera BioSync <avatariamfoundation@gmail.com>
2025-12-22 13:46:33 +0200

    Update NeuroGridCore.sol

--- contracts/NeuroGridCore.sol
+++ contracts/NeuroGridCore.sol
@@ -1,28 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
-contract NeuroGridCore {
-    event PostOpMonitoringRequested(
-        uint256 indexed caseId,
-        address indexed patient
-    );
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    event MonitoringResultSubmitted(
-        uint256 indexed caseId,
-        string resultHash
-    );
-
-    uint256 public caseCounter;
-
-    function requestPostOpMonitoring() external {
-        caseCounter += 1;
-        emit PostOpMonitoringRequested(caseCounter, msg.sender);
+contract NeuroGrid is Ownable {
+    struct ComputeTask {
+        string description;
+        address requester;
+        uint256 reward;
+        bool completed;
     }
 
-    function submitMonitoringResult(
-        uint256 caseId,
-        string calldata resultHash
-    ) external {
-        emit MonitoringResultSubmitted(caseId, resultHash);
+    mapping(uint256 => ComputeTask) public tasks;
+    uint256 public taskCount;
+    address public neuroToken;
+
+    constructor(address _neuroToken) {
+        neuroToken = _neuroToken;
+    }
+
+    function submitTask(string memory _description, uint256 _reward) public {
+        tasks[taskCount] = ComputeTask(_description, msg.sender, _reward, false);
+        taskCount++;
+    }
+
+    function completeTask(uint256 _taskId) public {
+        require(_taskId < taskCount, "Task does not exist");
+        require(!tasks[_taskId].completed, "Task already completed");
+        tasks[_taskId].completed = true;
+    }
+
+    function getTask(uint256 _taskId) public view returns (string memory, address, uint256, bool) {
+        ComputeTask memory task = tasks[_taskId];
+        return (task.description, task.requester, task.reward, task.completed);
     }
 }
