56bcb668b01d501ca63523c10b20aa5ca28da865
Aethera BioSync <avatariamfoundation@gmail.com>
2025-12-22 13:46:03 +0200

    Update MedToken.sol

--- contracts/MedToken.sol
+++ contracts/MedToken.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MedToken is ERC20, Ownable {
+    constructor() ERC20("NeuroGrid Token", "NGT") {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function rewardAgent(address agent, uint256 amount) public onlyOwner {
+        _mint(agent, amount);
+    }
+}
