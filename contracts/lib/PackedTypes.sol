ef7d8e3d2bad98e45a485d37daa605cc5349c7f2
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-01 22:58:25 +0200

    Update PackedTypes.sol

--- contracts/lib/PackedTypes.sol
+++ contracts/lib/PackedTypes.sol
@@ -1,1 +1,11 @@
+pragma solidity ^0.8.21;
+
+library PackedTypes {
+    struct ResultMeta {
+        uint64 modelId;
+        uint64 version;
+        uint64 epoch;
+        uint64 flags;
+    }
+}
 
