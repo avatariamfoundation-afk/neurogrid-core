29a41d9d23a0f919e45be9942c495189c0c9b8b4
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-01 15:36:12 +0200

    Update IValidatorModule.sol

--- contracts/interfaces/IValidatorModule.sol
+++ contracts/interfaces/IValidatorModule.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title IValidatorModule
+ * @notice Enforced submission boundary for validations
+ */
+interface IValidatorModule {
+    function submitValidation(
+        bytes32 artifactHash,
+        string calldata artifactDomain,
+        string calldata validationType,
+        bytes32 validatorSignature,
+        bool emergencyFlag
+    ) external;
+}
 
