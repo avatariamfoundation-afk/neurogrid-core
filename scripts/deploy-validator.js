ff9f5b95eb4973948f7a45202568489ef542ff1f
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-01 22:07:55 +0200

    Update deploy-validator.js

--- scripts/deploy-validator.js
+++ scripts/deploy-validator.js
@@ -1,1 +1,17 @@
+async function main() {
+  const coreAddress = process.env.CORE_ADDRESS;
+  if (!coreAddress) throw new Error("CORE_ADDRESS not set");
+
+  const Validator = await ethers.getContractFactory("ValidatorModule");
+  const validator = await Validator.deploy(coreAddress);
+
+  await validator.deployed();
+
+  console.log("ValidatorModule deployed to:", validator.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
