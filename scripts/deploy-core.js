66a1bdf8ae083a4a31282ab73ecbfdbf0941f664
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-01 21:59:23 +0200

    Update deploy-core.js

--- scripts/deploy-core.js
+++ scripts/deploy-core.js
@@ -1,1 +1,18 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying with:", deployer.address);
+
+  const Core = await ethers.getContractFactory("NeuroGridCore");
+  const core = await Core.deploy(deployer.address);
+
+  await core.deployed();
+
+  console.log("NeuroGridCore deployed to:", core.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
