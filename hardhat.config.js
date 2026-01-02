ae68f79f49d991ec0bd9a97ea48323d331558d9a
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-02 18:45:31 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,56 +1,37 @@
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
-
-module.exports = {
-  solidity: "0.8.19",
-  networks: {
-    testnet: {
-      url: process.env.BNB_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 20000000000,  // Optimized for lower costs
-
-      require("solidity-coverage");
-
 require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.19",
-  networks: {
-    bnbTestnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
-      chainId: 97
-    }
-  }
-};
-        
-module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    hardhat: {}
-  }
-};
-
-import "hardhat-gas-reporter";
-
-export default {
   solidity: {
     version: "0.8.21",
     settings: {
-      optimizer: { enabled: true, runs: 200 },
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
       viaIR: true
     }
   },
+
+  networks: {
+    hardhat: {},
+
+    bnbTestnet: {
+      url: process.env.BNB_RPC_URL || "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+      chainId: 97,
+      gasPrice: 20_000_000_000 // 20 gwei
+    }
+  },
+
   gasReporter: {
     enabled: true,
+    currency: "USD",
     noColors: true,
     showTimeSpent: true
   }
-};
-
-        
-    },
-  },
-  // Add for future scalability: plugins like hardhat-gas-reporter
 };
