f744cf412593873e5e28e1e5cc136a6b82d91ecc
Aethera BioSync <avatariamfoundation@gmail.com>
2026-01-04 14:55:38 +0200

    Update ComputeRegistry.sol

--- contracts/core/ComputeRegistry.sol
+++ contracts/core/ComputeRegistry.sol
@@ -1,7 +1,19 @@
-struct Node {
-  address operator;
-  uint256 stake;
-  uint256 reputation;
-  bool active;
+struct ComputeNode {
+    address operator;
+    uint256 stake;
+    uint256 reputation;
+    bool active;
 }
 
+Mandatory functions:
+registerNode()
+deactivateNode()
+getNode(address)
+
+Rules:
+stake > 0 required
+emit events
+fail loudly
+
+event NodeRegistered(address indexed operator);
+event NodeDeactivated(address indexed operator);
